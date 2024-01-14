783096de547753e10b230c96ab8a81f5ed243988
Daniele Conti <fourlastor@users.noreply.github.com>
2024-01-14 07:32:47 +0100

    Cleanup intro screen

--- core/src/main/java/io/github/fourlastor/game/di/modules/AssetsModule.java
+++ core/src/main/java/io/github/fourlastor/game/di/modules/AssetsModule.java
@@ -4,13 +4,8 @@ import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShaderProgram;
-import com.badlogic.gdx.utils.JsonReader;
 import dagger.Module;
 import dagger.Provides;
-import io.github.fourlastor.harlequin.animation.AnimationNode;
-import io.github.fourlastor.harlequin.loader.dragonbones.DragonBonesLoader;
-import io.github.fourlastor.harlequin.loader.spine.SpineLoader;
-import io.github.fourlastor.harlequin.loader.spine.model.SpineEntity;
 import javax.inject.Named;
 import javax.inject.Singleton;
 
@@ -18,34 +13,14 @@ import javax.inject.Singleton;
 public class AssetsModule {
 
     private static final String PATH_TEXTURE_ATLAS = "images/packed/images.pack.atlas";
-    private static final String PATH_LEVELS = "maps/levels.ldtk";
     private static final String PATH_WAVE_SHADER = "shaders/wave.vs";
     public static final String WHITE_PIXEL = "white-pixel";
-    private static final String PATH_SPINE_JSON = "animations/animation_spine.json";
-    private static final String PATH_DRAGON_BONES_JSON = "images/included/animations/dancer/dancer.json";
-
-    @Provides
-    public DragonBonesLoader dragonBonesLoader(JsonReader json) {
-        return new DragonBonesLoader();
-    }
-
-    @Provides
-    public SpineLoader spineLoader(JsonReader json) {
-        return new SpineLoader(json);
-    }
 
     @Provides
     @Singleton
-    public AssetManager assetManager(SpineLoader spineLoader, DragonBonesLoader dragonBonesLoader) {
+    public AssetManager assetManager() {
         AssetManager assetManager = new AssetManager();
-        assetManager.setLoader(SpineEntity.class, spineLoader);
-        assetManager.setLoader(AnimationNode.Group.class, dragonBonesLoader);
         assetManager.load(PATH_TEXTURE_ATLAS, TextureAtlas.class);
-        assetManager.load(PATH_SPINE_JSON, SpineEntity.class);
-        assetManager.load(
-                PATH_DRAGON_BONES_JSON,
-                AnimationNode.Group.class,
-                new DragonBonesLoader.Parameters(PATH_TEXTURE_ATLAS, "animations/dancer/texture"));
         assetManager.load(PATH_WAVE_SHADER, ShaderProgram.class);
         assetManager.finishLoading();
         return assetManager;
